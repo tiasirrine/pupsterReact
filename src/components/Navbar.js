2ef1f6d86476e86b8dda497b2446b08a5c0f95f2
Tia Sirrine <tiasirrine@outlook.com>
2018-06-11 21:22:32 -0700

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const NavTabs = props => (
+    <ul className="nav nav-tabs">
+        <li className="nav-item">
+            <a
+                onClick={() => props.handlePageChange("About")}
+                className={
+                    props.currentPage === "About" ? "nav-link active" : "nav-link"
+                }
+            >
+                About
+      </a>
+        </li>
+        <li className="nav-item">
+            <a
+                onClick={() => props.handlePageChange("Discover")}
+                className={
+                    props.currentPage === "Discover" ? "nav-link active" : "nav-link"
+                }
+            >
+                Discover
+      </a>
+        </li>
+        <li className="nav-item">
+            <a
+                onClick={() => props.handlePageChange("Search")}
+                className={
+                    props.currentPage === "Search" ? "nav-link active" : "nav-link"
+                }
+            >
+                Discover
+      </a>
+        </li>
+    </ul>
+);
+
+export default NavTabs;
