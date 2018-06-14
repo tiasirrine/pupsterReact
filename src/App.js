b856655446285c58592faecc8c5e2eb8360850f4
Trevor Johnson <johnsontrevor55@gmail.com>
2018-06-13 18:45:22 -0700

    route update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import About from './components/pages/About';
+import Discover from './components/pages/Discover';
+// import Search from './components/pages/Search';
+import NavTabs from './components/Navbar';
 
-const App = () => (
-  <Router>
-    <div>
-      <NavTabs />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/discover" component={Discover} />
-      <Route exact path="/search" component={Search} />
-    </div>
-  </Router>
-);
+class App extends React.Component {
+  state = {
+    currentPage: 'Home'
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <NavTabs />
+          <Route exact path="/" component={About} />
+          <Route exact path="/discover" component={Discover} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,30 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NavTabs = props => (
-    <ul className="nav nav-tabs">
-        <li className="nav-item">
-            <a
-                onClick={() => props.handlePageChange("About")}
-                className={
-                    props.currentPage === "About" ? "nav-link active" : "nav-link"
-                }
-            >
-                About
-      </a>
-        </li>
-        <li className="nav-item">
-            <a
-                onClick={() => props.handlePageChange("Discover")}
-                className={
-                    props.currentPage === "Discover" ? "nav-link active" : "nav-link"
-                }
-            >
-                Discover
-      </a>
-        </li>
-        <li className="nav-item">
-            <a
-                onClick={() => props.handlePageChange("Search")}
-                className={
-                    props.currentPage === "Search" ? "nav-link active" : "nav-link"
-                }
-            >
-                Search
-      </a>
-        </li>
-    </ul>
+const NavTabs = () => (
+  <ul className="nav nav-tabs">
+    <li className="nav-item">
+      <Link to="/" className={window.location.pathname === '/' ? 'nav-link active' : 'nav-link'}>
+        About
+      </Link>
+    </li>
+    <li className="nav-item">
+      <Link
+        to="/discover"
+        className={window.location.pathname === '/discover' ? 'nav-link active' : 'nav-link'}
+      >
+        Discover
+      </Link>
+    </li>
+    <li className="nav-item">
+      <Link
+        to="/search"
+        className={window.location.pathname === '/search' ? 'nav-link active' : 'nav-link'}
+      >
+        Search
+      </Link>
+    </li>
+  </ul>
 );
 
 export default NavTabs;
